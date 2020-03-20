35dc5818aae78afd797c419d80bd09847a19864c
yooyboys <46861244+yooyboys@users.noreply.github.com>
2020-03-21 01:59:38 +0800

    Add files via upload

---
+++ x.js
@@ -0,0 +1,15 @@
+// 根据屏幕尺寸  
+if (screen && screen.width > 480) {  
+document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/p2p.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/p2p.js"><\/script><script src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script><script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.0/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.0/hls.min.js"><\/script><script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/hls/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/hls/hls.min.js"><\/script>');
+
+}  
+
+// userAgent判断  
+if( /Android|webOS|iPhone|Linux|iPod|iPad|BlackBerry/i.test(navigator.userAgent)){  
+document.write('<script src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script><script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.0/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.0/hls.min.js"><\/script><script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/hls/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/hls/hls.min.js"><\/script>');
+
+}  
+
+// 调用
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/00-02.js"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/20-23.js"><\/script>');
