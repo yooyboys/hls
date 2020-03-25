dbb0a14bbce4c8b04f2b3d683495630121c8af23
yooyboys <46861244+yooyboys@users.noreply.github.com>
2020-03-25 17:22:14 +0800

    Update dy.js

--- dy.js
+++ dy.js
@@ -1,6 +1,6 @@
 // 调用
 document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/p2p.js"><\/script>');
-document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.2/hls.min.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.3/hls.min.js"><\/script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script>');
 document.write('<script src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/cdnbye@latest"><\/script>');
