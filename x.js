4eebb6704b26fabc1587e561ca9467c245563112
yooyboys <46861244+yooyboys@users.noreply.github.com>
2020-03-25 17:23:15 +0800

    Update x.js

--- x.js
+++ x.js
@@ -2,7 +2,7 @@
 if (screen && screen.width > 480) {  
 document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/p2p.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.9/dp/p2p.js"><\/script>');
 document.write('<script src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script>');
-document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.1/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.1/hls.min.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.3/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.3/hls.min.js"><\/script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script>');
 
 }  
@@ -10,7 +10,7 @@ document.write('<script src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js
 // userAgent判断  
 if( /Android|webOS|iPhone|Linux|iPod|iPad|BlackBerry/i.test(navigator.userAgent)){  
 document.write('<script src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.bootcss.com/hls.js/0.12.4/hls.min.js"><\/script>');
-document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.1/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.1/hls.min.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.3/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/hls@1.3/hls.min.js"><\/script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script>');
 
 }  
